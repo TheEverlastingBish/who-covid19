1f3f1beffb9de2e37b8a9c88171658ae545c1c44
TheEverlastingBish <sinha.bish@gmail.com>
2020-04-07 21:40:33 +0100

    Fixed dfs merge, added ignore_index=True

--- main.py
+++ main.py
@@ -14,32 +14,35 @@ url = r"https://www.who.int/emergencies/diseases/novel-coronavirus-2019/situatio
 html_content = requests.get(url).text
 soup = BeautifulSoup(html_content, "html.parser")
 
-print(soup.title.text)
 
-a_s = []
+pdf_links = []
 for a in soup.find_all('a', attrs = {"target": "_blank"}):
-    ap = re.search('.*situation-report.*', a['href'])
-    if ap:
-        a_s.append(a['href'])
+    sr = re.search('.*situation-report.*', a['href'])
+    if sr:
+        pdf_links.append(a['href'])
 
-
-clean_links = ['https://www.who.int' + a for a in a_s]
+clean_links = ['https://www.who.int' + a for a in pdf_links]
 latest_pdf = clean_links[0]
-print(latest_pdf)
 
-# latest_pdf = r".\data\20200405-sitrep-76-covid-19.pdf"
+# latest_pdf = r".\data\20200406-sitrep-77-covid-19.pdf"
 
+print("Latest PDF Link:", latest_pdf)
+
+
+
+# Read PDF Tables
 dfs = tabula.read_pdf(latest_pdf, 
                       pages='2-6', 
                       multiple_tables=True, 
-                      guess=True,
                       output_format='dataframe', 
-                      pandas_options={'encoding': 'utf-8'})
+                      pandas_options={'encoding': 'utf-8', 'header': None})
 
-print('#' * 30)
-print(len(dfs))
+print("Tables found:", len(dfs))
 
-df = pd.concat(dfs)
+
+# Generate Combined Pandas DF
+df = pd.concat(dfs, ignore_index=True)
+
 
 
 # Drop bad rows
@@ -49,14 +52,14 @@ rows_to_del = range(first_row)
 df = df.drop(rows_to_del).reset_index(drop=True)
 
 
+
 # Drop bad columns
 cols = df.columns.values
-expected_cols = [i for i in range(7)]
+expected_cols = range(7)
 cols_to_del = list(set(cols) - set(expected_cols))
 
 if len(df) > 6:
     df.drop(columns=cols_to_del, inplace=True)
-
 
 # Rename columns
 df.columns = ['report_country', 
@@ -67,8 +70,10 @@ df.columns = ['report_country',
               'trans_class', 
               'days_since_last_report']
 
+
 df.drop(df[df['report_country'].isin(['Subtotal for all', 'regions', 'Grand total', 'International', 'conveyance (Diamond'])].index, inplace=True)
 df.drop(df[df['confirmed'].isin(['Subtotal for all', 'regions', 'Grand total'])].index, inplace=True)
+
 
 
 # Cleaning
@@ -93,9 +98,9 @@ def clean_text(df):
 
 df = clean_text(df)
 
-
 # Repopulate col1 with slid values from col2
 df.loc[(df['report_country'].isnull()) | (df['report_country'] == 'nan'), 'report_country'] = df.loc[(df['report_country'].isnull()) | (df['report_country'] == 'nan'), 'confirmed']
+
 
 
 # Amend dtypes
@@ -116,12 +121,15 @@ region_labels = ['Western Pacific Region',
                  'Territories']
 
 df.drop(df[(~df['report_country'].isin(region_labels)) & (df['confirmed'].isnull())].index, inplace=True)
-print(df.shape)
+
+
 
 
 # Generate Region
 df['region'] = df.loc[(df['confirmed'].isnull()) & (df['report_country'] != 'Territories'), 'report_country']
 df['region'] = df['region'].fillna(method='ffill')
+
+
 
 
 # Extract Territories
@@ -137,6 +145,8 @@ df.dropna(subset=['report_country', 'confirmed'], inplace=True)
 df.drop(df[df['report_country'] == 'nan'].index, inplace=True)
 
 
+
+
 # Final Cleaning
 with open(r".\replacements.json", "r") as json_repls:
     repls = json.load(json_repls)
@@ -144,27 +154,27 @@ with open(r".\replacements.json", "r") as json_repls:
 df.replace(repls, inplace=True)
 df.drop(df[df['report_country'].str.isnumeric()].index, inplace=True)
 
-df['report_date'] = datetime.now().date()
+latest_file_name = os.path.basename(latest_pdf).split('.pdf')[0]
+file_date = datetime.strptime(re.match('\d{8}', latest_file_name).group(), "%Y%m%d")
+
+df['report_date'] = file_date
+
 new_order = ['report_date', 'region', 'location_type', 'report_country', 'confirmed', 'confirmed_new', 'deaths', 'deaths_new', 'trans_class', 'days_since_last_report']
+
 df = df.loc[:, new_order]
 df.fillna(value=0, inplace=True)
-print(df.shape)
+
+
 
 
 # Output
 repo_dir = os.path.dirname(os.path.abspath(__file__))
-print(repo_dir)
 data_dir = os.path.join(repo_dir, 'data')
-print(data_dir)
 
 if not os.path.exists(data_dir):
     os.mkdir(data_dir)
 
-latest_file_name = os.path.basename(latest_pdf).split('.pdf')[0]
-
-file_date = datetime.strptime(re.match('\d{8}', latest_file_name).group(), "%Y%m%d")
-output_file_name = latest_file_name + '_' + datetime.now().strftime("%Y-%m-%d") + '.csv'
-
+output_file_name = latest_file_name + '.csv'
 output_file = os.path.join(data_dir, output_file_name)
 print(output_file)
 
